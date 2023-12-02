6ab2d2d2313dbd6fe854a84a9cfbbaa4080b42d7
noah.osu <noah.osu@bcom.cm.cluster>
2023-12-01 22:38:06 -0600

    $ mpicc mpi.c $ mpirun_rsh -np 4 -ppn 1 ./a.out Sum: 4 $ mpirun_rsh -np 512 -ppn 128 ./a.out Sum: 512

--- 01/mpi.c
+++ 01/mpi.c
@@ -4,15 +4,21 @@
 int main(int c, char **v)
 {
     // Begin le MPI
-    int node;
-    int i = 0;
-    int max = 0;
-    MPI_Init(&c, &v);
-    MPI_Comm_rank(MPI_COMM_WORLD, &node);
+    int rank;
+    int res;
+    int size = 0;
+    int count = 0;
 
-    if (node == 0)
-        printf("I am big rank 0\n");
-    else
-        printf("I am rank: %d\n", node);
+    MPI_Init(&c, &v);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    count++;
+    MPI_Reduce(&count, &res, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+      printf("Sum: %d\n", res);
+    }
     MPI_Finalize();
+    return 0;
 }

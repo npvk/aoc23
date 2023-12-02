a575e64275276a1700dd78b2c738173d0c0159cd
noah.osu <noah.osu@bcom.cm.cluster>
2023-12-01 22:58:01 -0600

    MPI works
    noah.osu@bcom:~/src/aoc23/01$ mpirun_rsh -np 1 -ppn 1 ./a.out ./input.txt
    Sum: 54019
    noah.osu@bcom:~/src/aoc23/01$ mpirun_rsh -np 1 -ppn 4 ./a.out ./input.txt
    Sum: 54019
    noah.osu@bcom:~/src/aoc23/01$ mpirun_rsh -np 1 -ppn 128 ./a.out ./input.txt
    Sum: 54019
    noah.osu@bcom:~/src/aoc23/01$ scontrol show hostname
    node001
    node002
    node003
    node004
    noah.osu@bcom:~/src/aoc23/01$

--- 01/mpi.c
+++ 01/mpi.c
@@ -1,24 +1,162 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 #include <mpi.h>
+
+#define IN_LEN 2424
+
+int preprocess(char *p);
+int process(char *p);
+char check_num(char val, char *spell, char *in);
+char get_num(char *str);
+
+char *input[IN_LEN] = {NULL};
 
 int main(int c, char **v)
 {
-    // Begin le MPI
     int rank;
-    int res;
+    int total;
+    int count;
     int size = 0;
-    int count = 0;
 
+    if(c != 2) {
+        printf("WRONG ARGS: [filename]\n\n");
+        return -1;
+    }
+
+    count = preprocess(v[1]);
     MPI_Init(&c, &v);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    count++;
-    MPI_Reduce(&count, &res, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    // Get start, end
+    // per_array = count / size
+    // start idx = rank * per_array
+    // For i = start, i < end; i++
+    //   process
+    int local_sum = 0;
+    int per = count / size;
+    int start_idx = per * rank;
+    for(int i = start_idx; i < start_idx + per; i++) {
+      local_sum += process(input[start_idx+i]);
+    }
+    MPI_Reduce(&local_sum, &total, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
-      printf("Sum: %d\n", res);
+      printf("Sum: %d\n", total);
     }
+
     MPI_Finalize();
     return 0;
+}
+
+int preprocess(char *p) {
+    int fd;
+    int wc = 0;
+    fd = open(p, O_RDONLY);
+    struct stat s;
+    int status;
+    char *mem;
+
+    status = fstat(fd, &s);
+    mem = mmap(0, s.st_size, PROT_READ|PROT_WRITE, MAP_PRIVATE, fd, 0);
+    close(fd);
+    input[wc++] = &mem[0];
+    for(int i = 0; mem[i] != '\0'; i++) {
+        if(mem[i] == '\n') {
+            input[wc++] = &(mem[i+1]);
+            mem[i] = '\0';
+        }
+    }
+    return --wc;
+}
+
+int process(char* p) {
+    int set = 0;
+    char in[3];
+    int out = 0;
+    if( p == NULL)
+        return out;
+
+    while( *p != '\0') {
+        register char tmp;
+        tmp = get_num(p);
+        // found it
+        if(tmp != '\0') {
+            if (!set) {
+                in[0] = tmp;
+                in[1] = tmp;
+                set = 1;
+            } else {
+                in[1] = tmp;
+            }
+        }
+        // Next
+        p++;
+    }
+    in[2] = '\0';
+    return atoi(in);
+}
+
+
+// Check is a letter
+// Returns '\0' if not
+// Otherwise the char representing
+// the letter
+// o - search one
+// t - two, three
+// f - four, five
+// s - six, seven
+// e - eight
+// n - nine
+char get_num(char *str) {
+    char out = '\0';
+    // Sanity check
+    if (*str == out)
+        return out;
+    if (isdigit(*str)) {
+        out = *str;
+    } else if(isalpha(*str)) {
+        switch(*str) {
+        case 'o':
+            out = check_num('1', "one", str);
+            break;
+        case 't':
+            out = check_num('2', "two", str);
+            if (out == '\0')
+                out = check_num('3', "three", str);
+            break;
+        case 'f':
+            out = check_num('4', "four", str);
+            if (out == '\0')
+                out = check_num('5', "five", str);
+            break;
+        case 's':
+            out = check_num('6', "six", str);
+            if (out == '\0')
+                out = check_num('7', "seven", str);
+            break;
+        case 'e':
+            out = check_num('8', "eight", str);
+            break;
+        case 'n':
+            out = check_num('9', "nine", str);
+            break;
+        }
+    }
+    return out;
+}
+
+
+char check_num(char val, char *spell, char *in) {
+    register int len = strlen(spell);
+    if(strlen(in) >= len && strncmp(spell, in, len) == 0) {
+        return val;
+    }
+    return '\0';
 }
